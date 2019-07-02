1fb0bf05f3631e3bd86b9b8230f742eb466401f4
Rocky1981 <42718997+srikarkura@users.noreply.github.com>
2019-07-02 17:54:01 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,5 +6,6 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
 
